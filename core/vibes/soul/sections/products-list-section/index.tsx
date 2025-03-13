5eccb6ec198e88fbea6aec2d543c799ba36940b0
Kathiravan Balu <kathir@arizon.digital>
2025-03-13 19:26:10 +0530

    Revert "removing the category image"
    This reverts commit 0839a977736e69f724cc0585e285dca07fef98c3.

--- core/vibes/soul/sections/products-list-section/index.tsx
+++ core/vibes/soul/sections/products-list-section/index.tsx
@@ -68,8 +68,9 @@ export function ProductsListSection({
   emptyStateSubtitle,
   emptyStateTitle,
   placeholderCount = 8,
-  categoryBannerImage = "/api/placeholder/1200/300",
+  categoryBannerImage,
 }: Props) {
+
   return (
     <div className="group/products-list-section @container">
       <div className="mx-auto max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-12">
