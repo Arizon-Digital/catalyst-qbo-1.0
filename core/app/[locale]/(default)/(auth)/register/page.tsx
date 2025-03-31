011aa29cfbd8e90bc6eff7b99c6dd4ea7ab71680
priyanka-patra98 <priyanka@arizon.digital>
2025-03-31 18:56:18 +0530

    register build error

--- core/app/[locale]/(default)/(auth)/register/page.tsx
+++ core/app/[locale]/(default)/(auth)/register/page.tsx
@@ -1,19 +1,26 @@
+
+
+
+
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 import { getTranslations } from 'next-intl/server';
-import { Breadcrumbs as ComponentsBreadcrumbs } from '~/components/ui/breadcrumbs';
 
-import { bypassReCaptcha } from '~/lib/bypass-recaptcha';
+// TODO: Add recaptcha token
+// import { bypassReCaptcha } from '~/lib/bypass-recaptcha';
 
-import { RegisterCustomerForm } from './_components/register-customer-form';
+import { DynamicFormSection } from '@/arizon/soul/sections/dynamic-form-section';
+import { formFieldTransformer } from '~/data-transformers/form-field-transformer';
+import {
+  PASSWORD_FIELDS,
+  ALL_FIELDS,
+} from '~/data-transformers/form-field-transformer/utils';
+import { exists } from '~/lib/utils';
+
+import { registerCustomer } from './_actions/register-customer';
 import { getRegisterCustomerQuery } from './page-data';
 
-const FALLBACK_COUNTRY = {
-  entityId: 226,
-  name: 'United States',
-  code: 'US',
-};
-
-export async function generateMetadata() {
+export async function generateMetadata(): Promise<Metadata> {
   const t = await getTranslations('Register');
 
   return {
@@ -23,12 +30,6 @@ export async function generateMetadata() {
 
 export default async function Register() {
   const t = await getTranslations('Register');
-  const breadcrumbs: any = [
-    {
-      label: 'CREATE ACCOUNT',
-      href: '#',
-    },
-  ];
 
   const registerCustomerData = await getRegisterCustomerQuery({
     address: { sortBy: 'SORT_ORDER' },
@@ -39,39 +40,23 @@ export default async function Register() {
     notFound();
   }
 
-  const {
-    addressFields,
-    customerFields,
-    countries,
-    defaultCountry = FALLBACK_COUNTRY.name,
-    reCaptchaSettings,
-  } = registerCustomerData;
-
-  const reCaptcha = await bypassReCaptcha(reCaptchaSettings);
-
-  const {
-    code = FALLBACK_COUNTRY.code,
-    entityId = FALLBACK_COUNTRY.entityId,
-    statesOrProvinces,
-  } = countries.find(({ name }) => name === defaultCountry) || {};
-
+  const { addressFields, customerFields } = registerCustomerData;
   return (
-    <div className="mx-auto mb-10 register-page-parent text-base lg:w-2/3 pageheading" id='width'>
-     <div className='flex items-center justify-center'>
-      <ComponentsBreadcrumbs
-        className="login-div login-breadcrumb mx-auto  px-[1px]"
-        breadcrumbs={breadcrumbs}
-      />
-      </div>
-      <h1 className="text-2xl font-normal text-center mb-9 font-robotoslab">{t('heading')}</h1>
-      <RegisterCustomerForm
-        addressFields={addressFields}
-        customerFields={customerFields}
-        reCaptchaSettings={reCaptcha}
-        countries={countries}
-        defaultCountry={{ entityId, code, states: statesOrProvinces ?? [] }}
-      />
-    </div>
+    <DynamicFormSection
+      action={registerCustomer}
+      fields={[
+        ...addressFields
+          .filter((field) => ALL_FIELDS.includes(field.entityId))
+          .map(formFieldTransformer)
+          .filter(exists),
+        ...customerFields
+          .filter((field) => PASSWORD_FIELDS.includes(field.entityId))
+          .map(formFieldTransformer)
+          .filter(exists),
+      ]}
+      submitLabel={t('Form.submit')}
+      title={t('heading')}
+    />
   );
 }
 
