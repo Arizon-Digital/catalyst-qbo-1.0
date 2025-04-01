c095d48cdfe4ba92a81461bff0b3df25919bc4a5
priyanka-patra98 <priyanka@arizon.digital>
2025-04-02 00:58:12 +0530

    bottom gap

--- core/arizon/soul/pages/product/[slug]/_components/gallery/index.tsx
+++ core/arizon/soul/pages/product/[slug]/_components/gallery/index.tsx
@@ -45,7 +45,7 @@ export const Gallery = ({ product }: Props) => {
   const defaultImageIndex = images.findIndex((image) => image.isDefault);
 
   return (
-    <div className=" mb-10 sm:-mx-0 md:mb-12">
+    <div className=" sm:-mx-0 md:mb-12">
       <div className="lg:sticky lg:top-0">
         <ComponentsGallery
           defaultImageIndex={defaultImageIndex}
